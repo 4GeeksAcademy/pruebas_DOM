828389dd4e48b02d98902a7055f954c2af6f00e8
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-05-20 19:37:00 +0000

    pruebas

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { Carousel } from "bootstrap";
 import "../style/index.css";
 
 /**
@@ -22,20 +23,51 @@ import "../style/index.css";
         city: null
     }
  */
+
+/**
+ * Esta función render es propia de javascript y renderiza (construye) el html al que se haga mención.
+ * Recibe "variables" que son las que están declaradas más abajo. (haciendo log podemos ver cuales
+ * son esas variables).
+ *
+ */
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+
+  //empezamos a construir la variable cover, que al llevar dentro una condición, hace necesaria su creación.
+  //Van a recibir una etiqueta de tipo HTML desde 0 (no la trae del archivo) y le pasa valores a esa etiqueta,
+  //dentro del div tiene una etiqueta de imagen la cual recibe a través de <img src>.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+
+  //con este condicional validamos lo que tiene includeCover(variable de abajo) dentro del objeto variables (por defecto = true)
+  //le indicamos que si está en false debe crear un div vacío.
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  // Con el querySelector estamos captando una parte del código HTML, en concreto hace referencia
+  //al div que se identifica como "widget_content". En hmtl vemos que ese div no tiene nada, pero
+  // la información se encuentra aqui.
+  //Con el innerHTML sustituirá el contenido de lo que tenga el html a lo que le pasemos por aqui.
+
+  /**
+   * aqui se está "llamando" a la variable "cover" para que se pinte su contenido dentro del nuevo div creado. 
+   * se llama a lo que tenga avatarURL.
+   *   nombre que viene por defecto. Como está con un innerHTML al poner otro en el campo "Name" se cambiará.
+      con el dolar{variables.name} lo que hacemos es llamar a nuestra variable (que en principio está en null)
+      y cuando comience a escribir sustituirá lo que tenga en el nombre de la ficha.
+   */
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+
+          ${cover} 
+
+          
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+
+  
+        
+          <h1>${variables.name} ${variables.lastName} </h1>
+
+          <h2>${variables.role}</h2>
+       
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -47,29 +79,30 @@ function render(variables = {}) {
 }
 
 /**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
+ *Se llama a la función onload que es una función propia de javascript, que se va a ejecutar cuando se 
+ abra la aplicación, y recibe (a través de function) un objeto con sus atributos, y cada atributo tiene
+ key-value.  
  */
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
+
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+
+    name: "Nombre",
+
+    lastName: "Apellido",
+
+    role: "Selecciona"
   };
+
+  /************************************************************************************* */
+
   render(window.variables); // render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
